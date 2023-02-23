d9574b9779c3b36121aa6c5c508fc438f73af99c
Maksim <75714743+Maksim8811@users.noreply.github.com>
2023-02-23 19:22:49 +0200

    ver2 for dey 23

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -29,24 +29,4 @@ startBtn.addEventListener("click", clickStartBtn)
 stopBtn.addEventListener("click", clickStopBtn)
 
 
-function getDiscount(totalSpent) {
-  const BASE_DISCOUNT = 0;
-  const BRONZE_DISCOUNT = 0.02;
-  const SILVER_DISCOUNT = 0.05;
-  const GOLD_DISCOUNT = 0.1;
-  let discount;
-  // Change code below this line
-if (totalSpent >= 50000) {
- console.log(GOLD_DISCOUNT)
-} else if (totalSpent >= 20000 && totalSpent < 50000) {
-  console.log(SILVER_DISCOUNT)
-} else if (totalSpent >= 5000 && totalSpent < 20000) {
-  console.log(BRONZE_DISCOUNT)
-} else if (totalSpent < 5000) {
-  console.log(BASE_DISCOUNT) 
-}
 
-  // Change code above this line
-  
-}
-getDiscount(4000)
