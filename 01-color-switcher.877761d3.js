694683b60be6875f02efc7334424a52ffb560bf5
Maksim8811 <75714743+Maksim8811@users.noreply.github.com>
2023-02-23 17:34:43 +0000

    Deploying to gh-pages from @ Maksim8811/goit-js-hw-09@8875d774b179895a96d1270ed235b8d6b4e70fb4 🚀

---
+++ 01-color-switcher.877761d3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector('button[data-start="start"]'),r=document.querySelector('button[data-stop="stop"]');console.log(65);let o=null;r.setAttribute("disabled",!0);e.addEventListener("click",(()=>{o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),r.removeAttribute("disabled"),e.setAttribute("disabled",!0)})),r.addEventListener("click",(()=>{clearInterval(o),e.removeAttribute("disabled"),r.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.877761d3.js.map
